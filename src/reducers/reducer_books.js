cb7cc87aaaac62d1bf50b814f1b2e1cff2192a24
junhua <junhua.viva@gmail.com>
2017-05-29 17:02:10 +0800

    complete

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
     return [
-        {title: 'Javascript: The Good Parts'},
-        {title: 'Harry Potter'},
-        {title: 'The Dark Tower'},
-        {title: 'Eloquery Ruby'}
+        {title: 'Javascript: The Good Parts', pages: 101},
+        {title: 'Harry Potter', pages: 123},
+        {title: 'The Dark Tower', pages: 234},
+        {title: 'Eloquery Ruby', pages: 1}
     ]
 }
